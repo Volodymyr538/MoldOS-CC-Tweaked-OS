f5a8badbda921f7da4d763b594f08834770f6556
Volodymyr538 <horodnychyy_volodymyr@gradum103.ukr.education>
2026-07-13 08:14:16 +0300

    Update minesweeper.lua

--- apps/minesweeper.lua
+++ apps/minesweeper.lua
@@ -8,6 +8,9 @@ local GRID_W = math.min(W, 25)
 local GRID_H = math.min(H - 1, 12)
 local MINE_COUNT = math.floor(GRID_W * GRID_H * 0.15)
 
+local lang = dofile("/os/lib/lang.lua")
+local t = lang.t
+
 local function clear()
     term.setBackgroundColor(colors.black)
     term.setTextColor(colors.white)
@@ -136,7 +139,7 @@ local function draw()
     end
 
     term.setCursorPos(1, GRID_H + 1)
-    term.write("Left-click: reveal   Right-click: flag")
+    term.write(t("left_click_reveal"))
 end
 
 local function main()
@@ -180,10 +183,10 @@ local function main()
     term.setCursorPos(1, GRID_H + 1)
     if won then
         term.setTextColor(colors.lime)
-        term.write("You win! Click anywhere to exit...")
+        term.write(t("you_win"))
     else
         term.setTextColor(colors.red)
-        term.write("Boom! Click anywhere to exit...")
+        term.write(t("boom"))
     end
     term.setTextColor(colors.white)
     os.pullEvent("mouse_click")
